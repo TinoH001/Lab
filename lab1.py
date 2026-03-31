c333efc42726d5ff11fda4cfb8b90aef158a394d
Valentino <johannrk12@gmail.com>
2026-03-31 20:27:54 -0300

    Version_2

--- lab1.py
+++ lab1.py
@@ -23,17 +23,17 @@ for letra in texto: #Ciclo for
     if letra == "#" or letra == "*" or letra == "&" or letra == "!": #Condicion que se va a buscar
         copia = True #Copia se activa y empieza a duplicar digamos hasta que se vuelva False
         busca = letra #Lo que se esta buscando y funcionara como condicional
-    if copia: 
+    if copia:#Si es True
         if busca == "#" and letra not in ("012345678#"):#Si el prefijo es True y la letra no esta en ese mini diccionario, es True, da True y deja de copiar al ponerlo en False.
             copia = False #Deja de copiar
         elif busca == "*" and letra not in ("01*"):#Si el prefijo es True y la letra no esta en ese mini diccionario, es True, da True y deja de copiar al ponerlo en False.
-            copia = False
+            copia = False #Deja de copiar
         elif busca == "&" and letra not in ("01234567&"):#Si el prefijo es True y la letra no esta en ese mini diccionario, es True, da True y deja de copiar al ponerlo en False.
-            copia = False
+            copia = False #Deja de copiar
         elif busca == "!" and letra not in ("0123456789ABCDEF!"):#Si el prefijo es True y la letra no esta en ese mini diccionario, es True, da True y deja de copiar al ponerlo en False.
-            copia = False
-    if copia:
-        textv2.append(letra)
+            copia = False #Deja de copiar
+    if copia:#Si es True
+        textv2.append(letra)#Va agregando a la nueva version
 
 
 print(texto)
